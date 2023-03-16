015e5471dfc1e7555dbca248fb0feba701b01117
Sabera Mahmud Promee <mepromee@gmail.com>
2023-03-16 09:23:30 -0700

    Add middlewares
    - Global exception handler
    - Validate get posts request middleware

---
+++ AppServer/AppServer.Web/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text;
+using System;
+using System.Net.Http;
+using System.Net;
+
+namespace AppServer.Web.Middlewares
+{
+    public class GlobalExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionHandlerMiddleware> _logger;
+
+        public GlobalExceptionHandlerMiddleware(RequestDelegate next, ILogger<GlobalExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unhandled exception has occurred.");
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                await context.Response.Body.WriteAsync(Encoding.ASCII.GetBytes(ex.Message));
+
+            }
+        }
+    }
+}
---
+++ AppServer/AppServer.Web/Middlewares/ValidateGetPostsRequestMiddleware.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AppServer.Web.Middlewares
+{
+    public class ValidateGetPostsRequestMiddleware
+    {
+        private const string TargetUrl = "/api/posts";
+        private const string TagQueryParamName = "tags";
+        private const string SortByQueryParamName = "sortBy";
+        private const string DirectionQueryParamName = "direction";
+
+        private readonly RequestDelegate _next;
+
+        public ValidateGetPostsRequestMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Method == HttpMethods.Get && context.Request.Path.StartsWithSegments(TargetUrl, StringComparison.OrdinalIgnoreCase))
+            {
+                // Check if the 'tag' parameter is missing.
+                var tag = context.Request.Query[TagQueryParamName];
+                if (string.IsNullOrWhiteSpace(tag))
+                {
+                    await SetHttpErrorResponse(context, "tags parameter is required");
+                    return;
+                }
+
+                var sortBy = context.Request.Query[SortByQueryParamName];
+                // Check if the 'sortBy' parameter is missing or not valid.
+                if (!string.IsNullOrEmpty(sortBy) && !IsValidSortByParam(sortBy))
+                {
+                    await SetHttpErrorResponse(context, "sortBy parameter is invalid");
+                    return;
+                }
+
+                var direction = context.Request.Query[DirectionQueryParamName];
+                // Check if the 'direction' parameter is missing or not valid.
+                if (!string.IsNullOrEmpty(direction) && !IsValidDirectionParam(direction))
+                {
+                    await SetHttpErrorResponse(context, "direction parameter is invalid");
+                    return;
+                }
+            }
+            await _next(context);
+        }
+
+        // Check if the sortBy query param value is accepted or not.
+        private bool IsValidSortByParam(string sortBy)
+        {
+            var acceptableFields = new[] { "id", "reads", "likes", "popularity" };
+            return acceptableFields.Contains(sortBy.ToLower());
+        }
+
+        // Check if the direction query param value is accepted or not.
+        private bool IsValidDirectionParam(string direction)
+        {
+            var acceptableFields = new[] { "desc", "asc" };
+            return acceptableFields.Contains(direction.ToLower());
+        }
+
+        private async Task SetHttpErrorResponse(HttpContext context, string errorMessage)
+        {
+            var error = new { error = errorMessage };
+            var errorJson = JsonSerializer.Serialize(error);
+
+            // Set the content type of the response to JSON
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            await context.Response.WriteAsync(errorJson);
+        }
+    }
+}
