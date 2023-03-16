67d09f19e022f4e78d862736a504bf08edb6800d
Sabera Mahmud Promee <mepromee@gmail.com>
2023-03-16 09:27:48 -0700

    Implement post controller, update program.cs

---
+++ AppServer/AppServer.Web/Controllers/PostsController.cs
@@ -0,0 +1,28 @@
+﻿using AppServer.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AppServer.Web.Controllers
+{
+    [Route("api/posts")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly IPostService _postService;
+
+        public PostsController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        // GET: api/posts
+        [HttpGet]
+        public async Task<IActionResult> GetPosts([FromQuery] List<string> tags, [FromQuery] string? sortBy = "id", [FromQuery] string? direction = "asc")
+        {
+            var posts = await _postService.GetPostsAsync(tags, sortBy, direction);
+            return Ok(posts);
+        }
+    }
+}
--- AppServer/AppServer.Web/Program.cs
+++ AppServer/AppServer.Web/Program.cs
@@ -1,3 +1,8 @@
+using AppServer.Data.Models;
+using AppServer.Data.Services;
+using AppServer.Domain.Services;
+using AppServer.Web.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +12,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHttpClient();
+
+builder.Services.AddScoped<IPostService, PostService>();
+builder.Services.AddScoped<IThirdPartyDataAccessApiService<Post>, PostsDataService>();
+
+builder.Services.AddLogging();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,8 +29,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseMiddleware<ValidateGetPostsRequestMiddleware>();
+app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
 
-app.MapControllers();
+app.UseRouting();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
