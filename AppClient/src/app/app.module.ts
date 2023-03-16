267ef3b0dbf012e3e7484c6477bc75ebce2ae6df
Sabera Mahmud Promee <mepromee@gmail.com>
2023-03-16 12:39:54 -0700

    Create app client project

---
+++ AppClient/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './areas/home/home.component';
+import { ViewPostsTableComponent } from './areas/view-posts-table/view-posts-table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ViewPostsTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    FormsModule,
+    HttpClientModule,
+    MatSnackBarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
